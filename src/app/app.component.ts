29d64f3a42d021b45dd985ce1cc1ce29932e14df
YoussefGhrir <gheriryoussef@gmail.com>
2024-11-15 01:46:39 +0100

    amélioration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,44 +10,76 @@ import { etudiant } from './Etudiant';
 })
 export class AppComponent implements OnInit {
   title = 'tpfoyerFront';
-
   form: boolean = false;
   closeResult!: string;
   listetudiants: any;
-  etudiant!:any;
+  etudiant!: any;
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
 
-  constructor(private etudiantService: NomDuServiceService, private modalService: NgbModal ) { }
+  constructor(private etudiantService: NomDuServiceService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.getAlletudiant();
-    this.etudiant={
-      
-      idEtudiant: null,  
+    this.etudiant = {
+      idEtudiant: null,
       nomEtudiant: null,
-      prenomEtudiant: null,  
+      prenomEtudiant: null,
       cinEtudiant: null,
       dateNaissance: null,
-    }
+    };
   }
 
-  getAlletudiant(){
-    return this.etudiantService.getAlletudiant().subscribe(res=>{
+  getAlletudiant() {
+    return this.etudiantService.getAlletudiant().subscribe(res => {
       this.listetudiants = res;
     });
   }
+
   addetudiant(c: any) {
     this.etudiantService.addetudiant(c).subscribe(() => {
       this.getAlletudiant();
       this.form = false;
+      this.showMessage('Étudiant ajouté avec succès!', 'success');
+    }, () => {
+      this.showMessage("Erreur lors de l'ajout de l'étudiant.", 'error');
     });
   }
 
+  deleteEtudiant(id: number) {
+    this.etudiantService.deleteEtudiant(id).subscribe(() => {
+      this.getAlletudiant();
+      this.showMessage('Étudiant supprimé avec succès!', 'success');
+    }, () => {
+      this.showMessage("Erreur lors de la suppression de l'étudiant.", 'error');
+    });
+  }
+
+  showMessage(message: string, type: string) {
+    if (type === 'success') {
+      this.successMessage = message;
+    } else if (type === 'error') {
+      this.errorMessage = message;
+    }
+
+    setTimeout(() => {
+      this.successMessage = null;
+      this.errorMessage = null;
+    }, 3000);
+  }
+
+  closeAlert() {
+    this.successMessage = null;
+    this.errorMessage = null;
+  }
+
   open(content: any, action: any) {
-    if (action != null)
-      this.etudiant = action
-    else
+    if (action != null) {
+      this.etudiant = action;
+    } else {
       this.etudiant = new etudiant();
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    }
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
--- src/app/nom-du-service.service.ts
+++ src/app/nom-du-service.service.ts
@@ -8,15 +8,19 @@ import { etudiant } from './Etudiant';
 })
 export class NomDuServiceService {
 
- 
   readonly API_URL = 'http://192.168.1.200:8082/tpFoyer17/api/etudiants';
 
   constructor(private httpClient: HttpClient) { }
-  getAlletudiant() {
-    return this.httpClient.get(`${this.API_URL}/retrieveAllEtudiants`)
+
+  getAlletudiant(): Observable<any> {
+    return this.httpClient.get(`${this.API_URL}/retrieveAllEtudiants`);
   }
-  addetudiant(etudiant : any) {
-    return this.httpClient.post(`${this.API_URL}/addEtudiant`, etudiant)
+
+  addetudiant(etudiant: any): Observable<any> {
+    return this.httpClient.post(`${this.API_URL}/addEtudiant`, etudiant);
   }
-  
+
+  deleteEtudiant(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.API_URL}/removeEtudiant/${id}`);
+  }
 }
